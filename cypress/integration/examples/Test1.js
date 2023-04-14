d5da6a37529d08ce981cee7c3d9db3ff263241cd
Amr Aly <amr.aly57@yahoo.com>
2023-04-14 11:21:11 +0200

    Selected and verfied products count that their names starting with ca

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,5 +1,8 @@
 describe("My First Test", () => {
   it("My first test case", () => {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.get('input[type="search"').type("ca");
+    cy.wait(2000);
+    cy.get(".product:visible").should("have.length", 4);
   });
 });
