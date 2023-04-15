f57439bd75c81ae25351dc79a3f7e0e69e428f5a
Amr Aly <amr.aly57@yahoo.com>
2023-04-15 17:21:57 +0200

    Explored with checkbox, static dropdown and dynamic dropdown

--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -1,3 +1,26 @@
 describe("My Third Test Suite", () => {
-  it("exploring with checkboxes", () => {});
+  it("exploring with checkboxes", () => {
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+    // checkbox
+    cy.get("#checkBoxOption1")
+      .check()
+      .should("be.checked")
+      .should("have.value", "option1")
+      .uncheck()
+      .should("not.be.checked");
+
+    cy.get("input[type='checkbox']").check(["option1", "option3"]);
+
+    // static dropdown
+    cy.get("select").select("option2").should("have.value", "option2");
+
+    // dynamic dropdown
+    cy.get("#autocomplete").type("egy");
+    cy.get(".ui-menu-item div").each(($el, index, $list) => {
+      if ($el.text() === "Egypt") {
+        cy.wrap($el).click();
+      }
+    });
+    cy.get("#autocomplete").should("have.value", "Egypt");
+  });
 });
