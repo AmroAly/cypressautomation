a092e4c524b5721bd3bdea6826b537d1ccef62a4
Amr Aly <amr.aly57@yahoo.com>
2023-04-15 02:15:04 +0200

    Explored css locators, parent-child chaining, indexes and Logging

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -4,5 +4,28 @@ describe("My First Test", () => {
     cy.get('input[type="search"').type("ca");
     cy.wait(2000);
     cy.get(".product:visible").should("have.length", 4);
+    cy.get(".products").as("productLocator");
+    cy.get("@productLocator").find(".product").should("have.length", 4);
+    cy.get("@productLocator")
+      .find(".product")
+      .eq(1)
+      .contains("ADD TO CART")
+      .click()
+      .then(() => console.log("Logging....."));
+    // console.log("Logging.....");
+    cy.get("@productLocator")
+      .find(".product")
+      .each(($el, index, $list) => {
+        const productName = $el.find("h4.product-name").text();
+        if (productName.includes("Cashews")) {
+          cy.wrap($el).find("button").click();
+        }
+      });
+
+    cy.get(".brand.greenLogo").then((logo) => {
+      cy.log(logo.text());
+    });
+
+    cy.get(".brand.greenLogo").should("have.text", "GREENKART");
   });
 });
